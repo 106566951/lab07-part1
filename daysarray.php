113cc51a8ec1dc869cf87bc4a73f3879ba2f92b7
ANDY HUYNH <s4167827@student.rmit.edu.au>
2026-05-21 12:44:24 +1000

    initial changes

---
+++ daysarray.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+    meta charset = "utf-8"/>
+    <title>Using PHP variables, arrays and operators </title>
+
+</head>
+<body>
+    <h1>Days of the week</h1>
+    <php
+
+    $days =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday","Saturday" ];
